8868e12e01ba4eed4561f72f43f9bff599de2dc6
KruGong <139752381+KruGong356@users.noreply.github.com>
2024-10-07 22:16:42 +0700

    Add files via upload

---
+++ membeli_barang.py
@@ -0,0 +1,27 @@
+N, M = map(int, input().split())
+P = list(map(int, input().split()))
+C = list(map(int, input().split()))
+
+maks_P = max(P)
+maks_C = max(C)
+min_P = min(P)
+min_C = min(C)
+
+if all(p > 0 for p in P) and all(c > 0 for c in C):
+    positif_p = sum(P)
+    hasil = min_C - positif_p
+elif all(p < 0 for p in P) and all(c < 0 for c in C):
+    negatif_c = sum(C)
+    hasil = negatif_c - maks_P
+elif all(p < 0 for p in P) and any(c > 0 for c in C):
+    hasil = min_C - maks_P
+    if min_C > maks_P:
+        hasil = 0
+    else:
+        print(hasil)
+else:
+    positif_p = sum(p for p in P if p > 0)
+    negatif_c = sum(c for c in C if c < 0)
+    hasil = negatif_c - positif_p
+
+print(hasil)
